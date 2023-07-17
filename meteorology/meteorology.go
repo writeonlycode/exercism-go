847d6a5986938ffc94ee6cd9c6440eef039bcf5f
Iago Bozza <iagobozza@writeonlycode.io>
2023-07-17 01:32:44 +0000

    meteorology

---
+++ meteorology/meteorology.go
@@ -0,0 +1,77 @@
+package meteorology
+
+import (
+	"fmt"
+)
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+// String returns the string "°C" if the temperature unit is Celsius or "°F" if
+// the temperature unit is Fahrenheit.
+func (tu TemperatureUnit) String() string {
+	switch tu {
+	case Celsius:
+		return "°C"
+	case Fahrenheit:
+		return "°F"
+	default:
+		return ""
+	}
+}
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+// String returns a string with the numeric value for the temperature and the
+// temperature unit separated by a space.
+func (t Temperature) String() string {
+	return fmt.Sprintf("%d %s", t.degree, t.unit.String())
+}
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+// String returns the string "km/h" if the speed unit is kilometers per hour or
+// "mph" if the speed unit is miles per hour.
+func (su SpeedUnit) String() string {
+	switch su {
+	case KmPerHour:
+		return "km/h"
+	case MilesPerHour:
+		return "mph"
+	default:
+		return ""
+	}
+}
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+func (s Speed) String() string {
+	return fmt.Sprintf("%d %s", s.magnitude, s.unit.String())
+}
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
+
+func (md MeteorologyData) String() string {
+	return fmt.Sprintf("%s: %s, Wind %s at %s, %d%% Humidity", md.location, md.temperature.String(), md.windDirection, md.windSpeed.String(), md.humidity)
+}
