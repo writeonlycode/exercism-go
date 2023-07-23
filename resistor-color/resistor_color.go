778436f8b78e42f4c81a64f865aa0b8ad44d25c0
Iago Bozza <iagobozza@writeonlycode.io>
2023-07-23 05:24:02 +0000

    resistor color

---
+++ resistor-color/resistor_color.go
@@ -0,0 +1,42 @@
+// Package resistorcolor defines various functions useful to deal with
+// electronic color codes.
+package resistorcolor
+
+import (
+	"strings"
+)
+
+var resistance = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+// Colors returns the list of all colors that are associated with some
+// electronic color code.
+func Colors() []string {
+	return []string{
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	}
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	return resistance[strings.ToLower(color)]
+}
