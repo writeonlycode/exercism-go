189927280043b82780013a5027898408f8ba3a98
Iago Bozza <iagobozza@writeonlycode.io>
2023-08-03 01:19:42 +0000

    grains

---
+++ grains/grains.go
@@ -0,0 +1,29 @@
+// Package grains defines functions to calculate the number of grains in a
+// chessboard that has 64 squares, where square 1 has one grain, square 2 has
+// two grains, and so on.
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+// Square returns the number of grains in the given square.
+func Square(number int) (uint64, error) {
+	if number < 1 || 64 < number {
+		return 0, errors.New("Number must be between 1 and 64.")
+	}
+	result := math.Pow(2, float64(number-1))
+	return uint64(result), nil
+}
+
+// Total returns the total number of grains in the chessboard.
+func Total() uint64 {
+	var result uint64 = 0
+
+	for i := 0; i < 64; i++ {
+		result += uint64(math.Pow(2, float64(i)))
+	}
+
+	return result
+}
